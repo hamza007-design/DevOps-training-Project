7814ccf226b183b27ea628e1942af09901b098c7
Hamza Sultan <hamzasultan9662@gmail.com>
2024-07-11 12:02:07 -0400

    new Changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,27 @@
 terraform {
-    backend "s3" {
-    bucket         = "terrraform-state-bucket-01"  # Name of the S3 bucket where the state will be stored.
-    key            = "terraform/terraf.tfstate" # Path within the bucket where the state will be read/written.
-    region         = "us-east-1" # AWS region of the S3 bucket.
-}
-  
+  backend "s3" {
+    bucket = "terrraform-state-bucket-01"
+    key    = "terraform/terraf.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>3.5.0"
+      version = "~> 3.5.0"
     }
   }
 }
 
-
 provider "aws" {
   region = "us-east-1"
 }
-
-
 
 resource "aws_vpc" "my_vpc_terraform" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "Terraform_vpc"
   }
-
 }
 
 resource "aws_subnet" "PublicSubnetTerraform" {
@@ -35,6 +31,7 @@ resource "aws_subnet" "PublicSubnetTerraform" {
     Name = "public"
   }
 }
+
 resource "aws_subnet" "PrivateSubnetTerraform" {
   vpc_id     = aws_vpc.my_vpc_terraform.id
   cidr_block = "10.0.2.0/24"
@@ -67,7 +64,6 @@ resource "aws_route_table_association" "Subnet_Association" {
   subnet_id      = aws_subnet.PublicSubnetTerraform.id
   route_table_id = aws_route_table.my_route_table.id
 }
-
 
 resource "aws_security_group" "terraform_security_group" {
   name        = "allow_tls"
@@ -106,51 +102,44 @@ data "aws_ami" "ubuntu" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  
 
   owners = ["099720109477"] # Canonical
 }
- resource "aws_instance" "my_ec2_instance" {
+
+resource "aws_instance" "my_ec2_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.PublicSubnetTerraform.id # Place the instance in the public subnet
+  subnet_id     = aws_subnet.PublicSubnetTerraform.id
   vpc_security_group_ids = [aws_security_group.terraform_security_group.id]
   key_name = "my-new-keypair"
   associate_public_ip_address = true
-  
-  # Ensure you have this key pair created in AWS or add this field accordingly
 
   tags = {
     Name = "My-Ec2"
   }
+}
 
-  provisioner "local-exec" {
-    command = "cd ../Ansible && touch dynamic_inventory.ini"
-  }
-  #Create a local file resource for the PEM file
-
-# Run the Ansible playbook using a null resource
-
-
-  depends_on = [aws_instance.my_ec2_instance]
- }
 data "template_file" "inventory" {
   template = <<-EOT
   [ec2_instances]
-  ${aws_instance.my_ec2_instance.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=../Ansible/my-new-keypair.pem
+  ${aws_instance.my_ec2_instance.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=../Ansible/my-new-keypair.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'
   EOT
 }
+
 resource "local_file" "dynamic_inventory" {
-  depends_on = [aws_instance.my_ec2_instance]
   filename = "../Ansible/dynamic_inventory.ini"
-  content = data.template_file.inventory.rendered
+  content  = data.template_file.inventory.rendered
   provisioner "local-exec" {
     command = "chmod 400 ${local_file.dynamic_inventory.filename}"
   }
 }
+
 resource "null_resource" "run_ansible" {
-  depends_on = [ local_file.dynamic_inventory ]
   provisioner "local-exec" {
-     command = "ansible-playbook -i ../Ansible/dynamic_inventory.ini ../Ansible/Playbooks.yml -- "
-   }
+    command = "ansible-playbook -i ../Ansible/dynamic_inventory.ini ../Ansible/Playbooks.yml"
+  }
+  depends_on = [
+    aws_instance.my_ec2_instance,
+    local_file.dynamic_inventory
+  ]
 }
